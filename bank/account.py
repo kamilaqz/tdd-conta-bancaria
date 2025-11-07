2ae65ef04974b4a611226146c2847e4cf466651d
Kamila Queiroz <kamilapaqueiroz@gmail.com>
2025-11-07 19:39:26 -0300

    refatoracao do metodo de deposito com validacao de tipagem e de valores

--- bank/account.py
+++ bank/account.py
@@ -2,5 +2,9 @@ class Account:
     def __init__(self) -> None:
         self.balance: float = 0
     
-    def deposit(self, amount):
+    def deposit(self, amount: float) -> None:
+        if not isinstance(amount, (int, float)):
+            raise TypeError("O valor deve ser um número")
+        if amount <= 0:
+            raise ValueError("O valor do depósito deve ser maior que zero")
         self.balance += amount
